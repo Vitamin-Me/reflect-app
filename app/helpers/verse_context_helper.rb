bcc6cd197ce7335ed8bc4d2faec43f1e40241432
Vitamin-Me <dannylowell51@gmail.com>
2025-10-27 20:20:10 -0400

    Failsafe for Bible API

--- app/helpers/verse_context_helper.rb
+++ app/helpers/verse_context_helper.rb
@@ -2,17 +2,22 @@ require "yaml"
 
 module VerseContextHelper
   def verse_context(reference)
-    # Reload YAML on each request in development, cache in production
-    if Rails.env.development?
-      verse_data = YAML.load_file(Rails.root.join("config/verse_context.yml"))
-    else
-      @verse_data ||= YAML.load_file(Rails.root.join("config/verse_context.yml"))
-    end
+    data =
+      if Rails.env.development?
+        YAML.safe_load_file(Rails.root.join("config/verse_context.yml")) || {}
+      else
+        @verse_context_data ||= YAML.safe_load_file(Rails.root.join("config/verse_context.yml")) || {}
+      end
 
-    verse_data[reference] || {
-      "author" => "Unknown",
+    # Make lookups reliable
+    data = data.transform_keys(&:to_s)
+    data[reference.to_s] || {
+      "author"   => "Unknown",
       "audience" => "General believers",
-      "setting" => "Context unavailable for this verse."
+      "setting"  => "Context unavailable for this verse."
     }
+  rescue Errno::ENOENT => e
+    Rails.logger.error("Missing verse_context.yml: #{e.message}")
+    { "author"=>"Unknown", "audience"=>"General believers", "setting"=>"Context unavailable." }
   end
 end
